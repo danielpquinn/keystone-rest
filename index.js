106d94f84092b1f2ee9d3e8309c16ab72c68e43a
Dan Quinn <danthemilk@gmail.com>
2014-04-18 22:45:47 -0700

    Added more granular exposure of fields

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-var keystone = require('keystone');
+var _ = require('underscore'),
+  keystone = require('keystone');
 
 keystone.set('restRoutes', []);
 
-module.exports = function (keystoneList, methods) {
+module.exports = function (keystoneList, options) {
+
 
   // Handle errors
   var sendError = function (err, res) {
     res.status(400);
     res.json(err);
   };
+
+
+  // Filter out omitted fields
+  var removeOmitted = function (method, model) {
+    var object, omitted;
+
+    if (!options.omit) { return model; }
+    if (!options.omit[method]) {
+      return model;
+    } else {
+      omitted = options.omit[method];
+    }
+
+    // If model is a mongoose model instance, change it
+    // into a plain object so it's properties can be
+    // deleted.
+    object = model.toObject ? model.toObject() : model;
+
+    _.each(omitted, function (field) {
+      if (object[field]) { delete object[field]; }
+    });
+    return object;
+  };
+
   
   // Add get
   var addGet = function () {
@@ -20,7 +46,10 @@ module.exports = function (keystoneList, methods) {
 
         keystoneList.model.find().populate(populate).exec(function (err, response) {
           if (err) { sendError(err, res); return; }
-          res.json(response);
+          response = _.map(response, function (item) {
+            return removeOmitted('get', item);
+          });
+          res.json(removeOmitted('get', response));
         });
       }
     });
@@ -32,7 +61,7 @@ module.exports = function (keystoneList, methods) {
 
         keystoneList.model.findById(req.params.id).populate(populate).exec(function (err, response) {
           if (err) { sendError(err, res); return; }
-          res.json(response);
+          res.json(removeOmitted('get', response));
         });
       }
     });
@@ -47,9 +76,9 @@ module.exports = function (keystoneList, methods) {
         var item = new keystoneList.model(),
           updateHandler = item.getUpdateHandler(req, res);
 
-        updateHandler.process(req.body, function (err, response) {
+        updateHandler.process(removeOmitted('post', req.body), function (err, response) {
           if (err) { sendError(err, res); return; }
-          res.json(item);
+          res.json(removeOmitted('get', item));
         });
       }
     });
@@ -67,14 +96,14 @@ module.exports = function (keystoneList, methods) {
           if (err) { sendError(err, res); return; }
           var updateHandler = item.getUpdateHandler(req);
 
-          updateHandler.process(req.body, function (err, response) {
+          updateHandler.process(removeOmitted('put', req.body), function (err, response) {
             if (err) { sendError(err, res); return; }
 
             // Not sure if it's possible to populate mongoose models after
             // save, so get the document again and populate it.
             keystoneList.model.findById(req.params.id).populate(populate).exec(function (err, item) {
               if (err) { sendError(err, res); return; }
-              res.json(item);
+              res.json(removeOmitted('get', item));
             });
           });
         });
@@ -98,8 +127,8 @@ module.exports = function (keystoneList, methods) {
     });
   };
 
-  if (methods.indexOf('get') > -1) { addGet(); }
-  if (methods.indexOf('post') > -1) { addPost(); }
-  if (methods.indexOf('put') > -1) { addPut(); }
-  if (methods.indexOf('delete') > -1) { addDelete(); }
+  if (options.methods.indexOf('get') > -1) { addGet(); }
+  if (options.methods.indexOf('post') > -1) { addPost(); }
+  if (options.methods.indexOf('put') > -1) { addPut(); }
+  if (options.methods.indexOf('delete') > -1) { addDelete(); }
 };
